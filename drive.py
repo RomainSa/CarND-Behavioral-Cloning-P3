909d0c5e65dc7b3965159ba594bfa354de54c366
romain.savidan <romain.savidan@cdiscount.com>
2017-02-18 18:36:29 +0100

    Switched from RGB to YUV

--- drive.py
+++ drive.py
@@ -33,7 +33,7 @@ def telemetry(sid, data):
         speed = data["speed"]
         # The current image from the center camera of the car
         imgString = data["image"]
-        image = Image.open(BytesIO(base64.b64decode(imgString)))
+        image = Image.open(BytesIO(base64.b64decode(imgString))).convert('YCbCr')
         image_array = np.asarray(image)
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
         min_speed = 30
