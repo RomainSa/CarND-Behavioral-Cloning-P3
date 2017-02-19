b2506c385ec6517d6f664e814131af238f80a68b
RomainSa <carnd@ip-172-31-47-62.ec2.internal>
2017-02-19 21:59:13 +0000

    Working version on track1

--- parameters.py
+++ parameters.py
@@ -5,8 +5,8 @@ local_hostname = 'MacBook-Pro-de-Romain.local'
 
 # folders
 local_data_folder = '/Users/roms/GitHub/SDCND_T1_Simulator/data/'
-remote_data_folder = '/run/user/1001/data/'
-data_folders_list = ['udacity/', 'Smooth_driving2/', 'Recovering_from_left2/', 'Left_side_driving2/', 'Right_side_driving2/']
+remote_data_folder = '/home/carnd//data/'
+data_folders_list = ['udacity/',  'Left_side_driving2/', 'Right_side_driving2/']
 default_images_folder = 'IMG/'
 
 # file names
@@ -18,7 +18,5 @@ steering_variables = np.array(['img_center', 'img_left', 'img_right', 'steering_
 
 # urls
 urls_list = ['https://d17h27t6h515a5.cloudfront.net/topher/2016/December/584f6edd_data/data.zip',
-             'https://s3-us-west-2.amazonaws.com/carnd-rs/Smooth_driving2.zip',
-             'https://s3-us-west-2.amazonaws.com/carnd-rs/Recovering_from_left2.zip',
              'https://s3-us-west-2.amazonaws.com/carnd-rs/Left_side_driving.zip',
              'https://s3-us-west-2.amazonaws.com/carnd-rs/Right_side_driving.zip']
