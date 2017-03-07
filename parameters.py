ec6695c14b95a65bccc01aa4602d36437f8e54a9
romain.savidan <romain.savidan@cdiscount.com>
2017-03-07 22:26:37 +0100

    Update: parameters

--- parameters.py
+++ parameters.py
@@ -6,7 +6,7 @@ local_hostname = 'MacBook-Pro-de-Romain.local'
 # folders
 local_data_folder = '/Users/roms/GitHub/SDCND_T1_Simulator/data/'
 remote_data_folder = '/home/carnd//data/'
-data_folders_list = ['udacity/',  'Left_side_driving2/', 'Right_side_driving2/']
+data_folders_list = ['udacity/']
 default_images_folder = 'IMG/'
 
 # file names
@@ -17,6 +17,4 @@ right_images_pattern = 'IMG/right_'
 steering_variables = np.array(['img_center', 'img_left', 'img_right', 'steering_angle', 'throttle', 'brake', 'speed'])
 
 # urls
-urls_list = ['https://d17h27t6h515a5.cloudfront.net/topher/2016/December/584f6edd_data/data.zip',
-             'https://s3-us-west-2.amazonaws.com/carnd-rs/Left_side_driving.zip',
-             'https://s3-us-west-2.amazonaws.com/carnd-rs/Right_side_driving.zip']
+urls_list = ['https://d17h27t6h515a5.cloudfront.net/topher/2016/December/584f6edd_data/data.zip']
